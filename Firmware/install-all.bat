5ebe9517f6eda2ea264eba11b63f32703108d939
istvan <istvan.herbak@gmail.com>
2022-12-19 14:04:25 +0100

    reverting to esptool 3.3 as 4.0 incorrectly flashes the device

--- Firmware/install-all.bat
+++ Firmware/install-all.bat
@@ -6,10 +6,10 @@ set BaseArgs=--chip esp32 --baud 921600
 set SetupArgs=--before default_reset --after hard_reset write_flash -z --flash_mode keep --flash_freq keep --flash_size detect --erase-all
 
 set Bootloader=0x1000 common\bootloader_dout_40m.bin
+set Partitions=0x8000 %BuildDir%\partitions.bin
 set Bootapp=0xe000 common\boot_app0.bin
 set Firmware=0x10000 %BuildDir%\firmware.bin
-set Partitions=0x8000 %BuildDir%\partitions.bin
-set Spiffs=0x310000 %BuildDir%\spiffs.bin
+set Spiffs=0x670000 %BuildDir%\spiffs.bin
 
 echo %EsptoolPath% %BaseArgs% %SetupArgs% %Bootloader% %Bootapp% %Firmware% %Partitions% %Spiffs%
 %EsptoolPath% %BaseArgs% %SetupArgs% %Bootloader% %Bootapp% %Partitions% %Firmware% %Spiffs%
--- Firmware/install-spiffs-only.bat
+++ Firmware/install-spiffs-only.bat
@@ -5,7 +5,7 @@ set BuildDir=.pio\build\release
 set BaseArgs=--chip esp32 --baud 921600
 set SetupArgs=--before default_reset --after hard_reset write_flash -z --flash_mode keep --flash_freq keep --flash_size detect
 
-set Spiffs=0x310000 %BuildDir%\spiffs.bin
+set Spiffs=0x670000 %BuildDir%\spiffs.bin
 
 echo %EsptoolPath% %BaseArgs% %SetupArgs% %Spiffs%
 %EsptoolPath% %BaseArgs% %SetupArgs% %Spiffs%
